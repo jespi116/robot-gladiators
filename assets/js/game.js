b252651db7d471fef7a30ecd659c0aa2d65caa4b
jespi116 <jespi116@fiu.edu>
2020-08-18 18:14:51 -0400

    mvp

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,45 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+console.log(enemyName,enemyHealth, enemyAttack);
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+
+    enemyHealth = enemyHealth - playerAttack;
+
+console.log(
+    playerName + " attacked " + enemyName + ". " + " now has " + enemyHealth + " health remaining.");
+
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth - enemyAttack;
+
+console.log(
+        enemyName + " attacked " + playerName + ". " + " now has " + playerHealth + " health remaining.");
+
+        if 
+        (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+        } 
+
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
+
+}
+
+fight();
