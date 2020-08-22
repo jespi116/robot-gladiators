a18f9c10ca4e067c8337ee3f5c94e12d7473e53c
jespi116 <jespi116@fiu.edu>
2020-08-22 17:07:21 -0400

    name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,6 +76,7 @@ var startGame = function() {
     for(var i = 0; i < enemyInfo.length; i++){
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + ( i + 1));
+            debugger;
     
             var pickedEnemyObj = enemyInfo[i];
 
@@ -156,8 +157,19 @@ var randomNumber = function (min, max) {
     return value;
 }
 
+var getPlayerName = function() {
+    var name = "";
+
+    while(name === "" || name === null) {
+        name = prompt("what is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
